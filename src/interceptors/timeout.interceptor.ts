8314ede70de20717d7c7d19ac8659fdfb0432541
Muhammad <muhammadakbar509@gmail.com>
2022-05-06 20:53:33 +0800

    longer timeout

--- src/interceptors/timeout.interceptor.ts
+++ src/interceptors/timeout.interceptor.ts
@@ -12,7 +12,7 @@ import { catchError, timeout } from 'rxjs/operators';
 export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      timeout(60000),
+      timeout(60000 * 15),
       catchError((err) => {
         if (err instanceof TimeoutError) {
           return throwError(() => new RequestTimeoutException());
